06c742bc491d65762f836c9171e63437d7b69a9f
Johan <johangeorge2002@gmail.com>
2026-01-04 15:51:48 +0530

    Done learning progress with basic controllers

--- src/main/java/com/capstone/learningprogress/LearningProgressApplication.java
+++ src/main/java/com/capstone/learningprogress/LearningProgressApplication.java
@@ -2,8 +2,12 @@ package com.capstone.learningprogress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class LearningProgressApplication {
 
 	public static void main(String[] args) {
